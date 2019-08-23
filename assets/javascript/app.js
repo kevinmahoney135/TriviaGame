167e1b351266fbbc9f0fbe7993ca05da7237371f
Kevin Mahoney <51928306+kevinmahoney135@users.noreply.github.com>
2019-08-23 15:11:32 -0500

    Added bg image to body style css. Added base html for game. Made an array of objects instead of just individual objects.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,41 @@
-var q1 = {
+var questionArray = [
+{
     question: "In the year 1900 in the U.S. what were the most popular first names given to boy and girl babies?",
-    answers: ["William and Elizabeth","Joseph and Catherine","John and Mary","George and Anne"],
-    correct: 2
-}
-var q2 = {
+    answers: ["William and Elizabeth",
+              "Joseph and Catherine",
+              "John and Mary",
+              "George and Anne"],
+    correct: "John and Mary"
+},
+{
     question: "When did the Liberty Bell get its name?",
-    answers: ["When it was made, in 1701","When it rang on July 4, 1776","In the 19th century, when it became a symbol of the abolition of slavery","None of the above"],
-    correct: 2
-}
-var q3 = {
+    answers: ["When it was made, in 1701",
+              "When it rang on July 4, 1776",
+              "In the 19th century, when it became a symbol of the abolition of slavery",
+              "None of the above"],
+    correct: "In the 19th century, when it became a symbol of the abolition of slavery"
+},
+{
     question: "In the Roy Rogers -Dale Evans Museum, you will find Roy and Dales stuffed horses. Roy's horse was named Trigger, which was Dales horse?",
-    answers: ["Buttermilk","Daisy","Scout","Tulip"],
-    correct: 0
-}
-var q4 = {
+    answers: ["Buttermilk",
+              "Daisy",
+              "Scout",
+              "Tulip"],
+    correct: "Buttermilk"
+},
+{
     question: "The Daniel Boon museum at the home where he died can best be described how?",
-    answers: ["A log cabin in Kentucky","A two-story clapboard house in Tennessee","A four-story Georgian-style home in Missouri","A three story brick house in Arkansas"],
-    correct: 2
-}
-var q5 = {
+    answers: ["A log cabin in Kentucky",
+              "A two-story clapboard house in Tennessee",
+              "A four-story Georgian-style home in Missouri",
+              "A three story brick house in Arkansas"],
+    correct: "A four-story Georgian-style home in Missouri"
+},
+{
     question: "Which of the following items was owned by the fewest U.S. homes in 1990?",
-    answers: ["Home computer","Compact disk player","Cordless phone","Dishwasher"],
-    correct: 1
-}
+    answers: ["Home computer",
+              "Compact disk player",
+              "Cordless phone",
+              "Dishwasher"],
+    correct: "Compact disk player"
+}];
