d74eaaf6baedb93b2690de97330bf4d319e702b6
Kevin Mahoney <51928306+kevinmahoney135@users.noreply.github.com>
2019-08-23 16:38:49 -0500

    Added javascript game logic.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -39,3 +39,67 @@ var questionArray = [
               "Dishwasher"],
     correct: "Compact disk player"
 }];
+
+var timer;
+var numCorrect = 0;
+var numWrong = 0;
+var timeLeft = 60;
+
+$(document).on("click", ".btn", function() {
+    if ($(this).val() === "start") {
+        startGame();
+    }
+
+    if ($(this).val() === "done") {
+        endGame();
+    }
+
+});
+
+function startGame() {
+    timer = setInterval(gameTimer, 1000);
+    var countDownTimer = $("<h2>Time Remaining: <span id='countDownTimer'>60</span> Seconds</h2>");
+    $(".game-section").empty();
+    $(".game-section").prepend(countDownTimer);
+
+    for (var i = 0; i < questionArray.length; i++) {
+        $(".game-section").append("<p>" + questionArray[i].question + "</p>");
+        for (var i2 = 0; i2 < questionArray[i].answers.length; i2++) {
+            $(".game-section").append("<input type='radio' name='q" + i + "' value='" + questionArray[i].answers[i2] + "''>" + questionArray[i].answers[i2]);
+        }
+    }
+
+    $(".game-section").append("<button class='btn' value='done'>Done</button>");
+}
+
+function endGame() {
+
+    for (var i = 0; i < questionArray.length; i++) {
+        $.each($("input[name='q" + i + "']:checked"), function() {
+            if ($(this).val() === questionArray[i].correct) {
+                numCorrect++;
+            }
+            else {
+                numWrong++;
+            }
+        });    
+    }
+
+    clearInterval(timeLeft);
+
+    $(".game-section").empty();
+
+    $(".game-section").html("<h2>All Done!</h2>");
+    $(".game-section").append("<p>Correct Answers: " + numCorrect + "</p>");
+    $(".game-section").append("<p>Incorrect Answers: " + numWrong + "</p>");
+    $(".game-section").append("<p>Unanswered: " + (questionArray.length - (this.numWrong + this.numCorrect)) + "</p>");
+}
+
+function gameTimer() {
+    timeLeft--;
+    $("#countDownTimer").html(timeLeft);
+    if (timeLeft === 0) {
+        endGame();
+    }
+}
+
